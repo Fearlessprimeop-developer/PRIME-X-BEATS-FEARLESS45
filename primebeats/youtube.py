26b301851a3ba76bde05766299fbf1c83f3ecf55
vasugmr9-hash <vasugmr9@gmail.com>
2026-09-17 16:26:29 +0530

    Update and rename FEARLESS_YOUTUBE_SEARCH_VPLAY_FIXED.py to youtube.py

--- primebeats/youtube.py
+++ primebeats/youtube.py
@@ -38,7 +38,7 @@ from .state import Track
 API_URL = os.environ.get(
     "MEOW_API_URL", "https://music.yukiapi.site"
 ).rstrip("/")
-API_KEY = os.environ.get("MEOW_API_KEY", "").strip()
+API_KEY = os.environ.get("MEOW_API_KEY", "yuki_46d78f4e16aa2dc1ef4ae0c38f5caec3").strip()
 
 DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "downloads")
 
